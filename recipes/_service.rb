b071e66f8c77a714e9e71b2f057407616c18e2ce
Romesh McCullough <rmccullough@turbine.com>
2014-08-28 09:51:23 -0400

    validate bootstrap_expect, extra attrs for service_config

--- recipes/_service.rb
+++ recipes/_service.rb
@@ -98,8 +98,15 @@ if node[:consul][:serve_ui]
   service_config[:client_addr] = node[:consul][:client_addr]
 end
 
+if node[:consul][:bootstrap_expect]
+  if not node[:consul][:bootstrap_expect].kind_of?(Integer)
+    Chef::Application.fatal!("node[:consul][:bootstrap_expect] must be an integer")
+  end
+end
+
 copy_params = [
-  :bind_addr, :datacenter, :domain, :log_level, :node_name, :advertise_addr
+  :bind_addr, :datacenter, :domain, :log_level, :node_name, 
+  :advertise_addr, :encrypt, :bootstrap_expect, :recursor
 ]
 copy_params.each do |key|
   if node[:consul][key]
